7892d595c8bca4c6a2d82331cbdafd8d4a8f2599
Alen <k45sp3rhow@gmail.com>
2020-03-13 00:55:21 +0700

    not found page

---
+++ src/views/Page404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Grid, Typography } from '@material-ui/core'
+
+export default function Page404 (props) {
+  return (
+    <Grid container justify='center' component={Container} style={{marginTop:'200px'}}>
+      <Typography variant='h3'>
+        <strong>404</strong> Page Not Found
+      </Typography>
+    </Grid>
+  )
+}
