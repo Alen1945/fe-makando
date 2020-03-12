0e03cdaa224cae0f72bd794eb982e8d6d6702f94
Alen <k45sp3rhow@gmail.com>
2020-03-13 05:10:20 +0700

    add validasi form user

--- src/views/DashboardAdmin/User.js
+++ src/views/DashboardAdmin/User.js
@@ -1,87 +1,45 @@
 import React from 'react'
-import {Grid, Box, Card, CardHeader, CardContent, CardActions, MenuItem, Button, TextField} from '@material-ui/core'
+import {Formik} from 'formik'
+import * as Yup from 'yup'
+import FormUser from './components/FormUser'
 
-export default function DashboardAdmin (props) {
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e.target)
-    const data = new FormData(e.target)
-    console.log(data)
-  }
+const initialFormUser = { fullname: '', username: '', password: '', email: '', gender: 'male', role: 1 }
+const msgRequired ='This is Required'
+const validationFormUser = Yup.object({
+  fullname: Yup.string().required(msgRequired),
+  username: Yup.string()
+    .min(6, 'Must be 6 characters or more')
+    .required(msgRequired),
+  password: Yup.string()
+    .min(8, 'Must be 8 characters or more')
+    .required(msgRequired),
+  email: Yup.string().email('Invalid email address')
+    .required(msgRequired),
+  gender: Yup.string()
+    .oneOf(
+      ['male', 'female'],
+      'Invalid Gender'
+    )
+    .required(msgRequired),
+  role: Yup.number()
+    .oneOf(
+      [1, 2, 3],
+      'Invalid Role'
+    )
+    .required(msgRequired),
+})
+export default function User (props) {
   return (
-    <form
-      autoComplete='off'
-      noValidate
-      onSubmit={handleSubmit}
+    <Formik
+      initialValues={initialFormUser}
+      validationSchema={validationFormUser}
+      onSubmit={async (values, form) => {
+        console.log(values)
+        console.log(form)
+        form.setSubmitting(false)
+      }}
     >
-      <Card elevation={0}>
-        <CardHeader title='Adding Users' titleTypographyProps={{variant: 'h5', align: 'center'}}/>
-        <CardContent>
-          <Grid container justify='center'>
-            <Grid container sm={8} item spacing={2}> 
-              <Grid item md={4} xs={6}>
-                <TextField fullWidth label='Full Name' margin='dense' name='fullname' type='text' required variant='outlined' />
-              </Grid>
-              <Grid item md={4} xs={6}>
-                <TextField fullWidth label='Username' margin='dense' name='username' type='text' required variant='outlined' />
-              </Grid>
-              <Grid item md={4} xs={6}>
-                <TextField fullWidth label='Password' margin='dense' name='password' type='password' required variant='outlined' />
-              </Grid>
-              <Grid item md={4} xs={6}>
-                <TextField fullWidth label='E-Mail' margin='dense' name='email' type='text' required variant='outlined' />
-              </Grid>
-              <Grid item md={4} xs={6}>
-                <TextField
-                  fullWidth
-                  label='Gender'
-                  margin='dense'
-                  name='gender'
-                  select
-                  variant='outlined'
-                >
-                  {
-                    [{ label: 'Male', value: 'male' },
-                      { label: 'Female', value: 'female' }].map(option => (
-                      <MenuItem key={option.value} value={option.value} >
-                        {option.label}
-                      </MenuItem>
-                    ))
-                  }
-                </TextField>
-              </Grid>
-              <Grid item md={4} xs={6}>
-                <TextField
-                  fullWidth
-                  label='Role'
-                  margin='dense'
-                  name='role'
-                  select
-                  variant='outlined'
-                >
-                  {
-                    [{label:'User',value:1},
-                      {label:'Admin',value:2},
-                      {label:'SuperAdmin',value:3}].map(option => (
-                      <MenuItem key={option.value} value={option.value} >
-                        {option.label}
-                      </MenuItem>
-                    ))
-                  }
-                </TextField>
-              </Grid>
-            </Grid>
-          </Grid>
-        </CardContent>
-        <CardActions>
-          <Grid container justify='center'>
-            <Button color='primary' variant='contained' type='submit' >
-              Save details
-            </Button>
-          </Grid>
-        </CardActions>
-      </Card>
-    </form>
+      <FormUser />
+    </Formik>
   )
 }
-
