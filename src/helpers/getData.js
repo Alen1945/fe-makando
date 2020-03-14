ffa1d3347eb7e22e812c810edadbd80c0fb1248f
Alen <k45sp3rhow@gmail.com>
2020-03-15 00:19:33 +0700

    add token on axios

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-
+import cookie from 'js-cookie'
 function getData (dataUrl, dataForm) {
   return new Promise((resolve, reject) => {
+    const config = {}
+    if (cookie.get('tokenm4k4nd0')) {
+      console.log(cookie.get('tokenm4k4nd0'))
+      config.headers = { Authorization: `Bearer ${cookie.get('tokenm4k4nd0')}` }
+    }
     const url = process.env.REACT_APP_API_URL + dataUrl
-    axios.get(url).then(result => {
+    axios.get(url, config).then(result => {
       resolve(result)
     }).catch((e) => {
       reject(e)
--- src/helpers/submitData.js
+++ src/helpers/submitData.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-
+import cookie from 'js-cookie'
 function submitData (dataUrl, dataForm) {
+  const config = {}
+  if (cookie.get('tokenm4k4nd0')) {
+    console.log(cookie.get('tokenm4k4nd0'))
+    config.headers = { Authorization: `Bearer ${cookie.get('tokenm4k4nd0')}` }
+  }
   return new Promise((resolve, reject) => {
     const url = process.env.REACT_APP_API_URL + dataUrl
-    axios.post(url, dataForm).then(result => {
+    axios.post(url, dataForm,config).then(result => {
       resolve(result)
     }).catch((e) => {
       reject(e)
