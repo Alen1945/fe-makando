f1b8fde0b37c6bcce7dc5393b0fe659a2feb7480
Alen <k45sp3rhow@gmail.com>
2020-03-18 16:09:14 +0700

    update total items

--- src/views/Carts/components/CartItems.js
+++ src/views/Carts/components/CartItems.js
@@ -2,24 +2,34 @@ import React from 'react'
 import {
   Container, Grid, Table, TableContainer, TableHead,
   TableRow, TableBody, TableCell, Avatar, IconButton,
-  Button, TextField
+  Button, TextField, Dialog, DialogContent, DialogContentText, DialogActions
 } from '@material-ui/core'
 import { Edit, Delete, Check } from '@material-ui/icons'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import CustomTextField from '../../../components/CustomTextField'
 import deleteData from '../../../helpers/deleteData'
+import patchData from '../../../helpers/patchData'
 import AlertDelete from '../../../components/AlertDelete'
 
 export default function CartItems (props) {
   const { setMsg } = props
-  const handleClick = (e) => {
-    props.setActiveStep(1)
-  }
+  const [openForm, setOpenForm] = React.useState(0)
+  const [initialValues, setInitialValues] = React.useState({
+    id: 0,
+    total_items: 0
+  })
   const [itemCart, setItemCart] = React.useState([])
   const [openDialogDelete, setOpenDialogDelete] = React.useState(0)
   const [deleteId, setDeleteId] = React.useState(0)
 
+  const handleOpenForm = (id, totals) => {
+    setInitialValues({ id: id, total_items: totals })
+    setOpenForm(1)
+  }
+  const handleClick = (e) => {
+    props.setActiveStep(1)
+  }
   const handleOpenDialogDelete = (id) => {
     setDeleteId(id)
     setOpenDialogDelete(1)
@@ -29,6 +39,8 @@ export default function CartItems (props) {
       const response = await deleteData(`/carts/${id}`)
       if (response.data.success) {
         setOpenDialogDelete(0)
+        setMsg({ display: 1, success: response.data.success, message: response.data.msg })
+      } else {
         setMsg({ display: 1, success: response.data.success, message: response.data.msg })
       }
     } catch (e) {
@@ -49,6 +61,49 @@ export default function CartItems (props) {
         onCancel={() => setOpenDialogDelete(0)}
         onDelete={() => deleteCart(deleteId)}
       />
+      <Dialog
+        open={openForm}
+        maxWidth='sm'
+        fullWidth='md'
+        onClose={() => setOpenForm(0)}
+      >
+        <DialogContentText align='center' style={{ marginTop: '10px' }}> Edit Total Items </DialogContentText>
+        <DialogContent>
+          <Formik
+            enableReinitialize
+            initialValues={initialValues}
+            validationSchema={
+              !initialValues.id ? Yup.object({ total_items: Yup.number().required() }) : Yup.object({ total_items: Yup.number().nullable() })
+            }
+            onSubmit={async (values) => {
+              try {
+                const response = await patchData('/carts/' + values.id, { total_items: values.total_items })
+                if (response.data.success) {
+                  setOpenForm(0)
+                  setMsg({ display: 1, success: response.data.success, message: response.data.msg })
+                } else {
+                  setMsg({ display: 1, success: response.data.success, message: response.data.msg })
+                }
+              } catch (e) {
+                console.log(e)
+                setMsg({ display: 1, success: e.response.data.success, message: e.response.data.msg })
+              }
+            }}
+          >
+            <Form align='center' style={{ marginTop: '10px' }}>
+              <CustomTextField size='small' type='number' name='total_items' label='Total Items' variant='outlined' component={TextField} />
+              <Button size='small' type='submit'>
+                <Check />
+              </Button>
+            </Form>
+          </Formik>
+        </DialogContent>
+        <DialogActions>
+          <Button color='primary' onClick={() => setOpenForm(0)}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Grid container justify='center' component={Container}>
         <Grid item sm={10} md={8}>
           <TableContainer>
@@ -66,7 +121,7 @@ export default function CartItems (props) {
                 {itemCart.map((cart) => (
                   <TableRow key={cart._id}>
                     <TableCell component='th' scope='row'>
-                      <IconButton><Edit /></IconButton>&nbsp;&nbsp;
+                      <IconButton onClick={() => handleOpenForm(cart._id, cart.total_items)}><Edit /></IconButton>&nbsp;
                       <IconButton onClick={() => handleOpenDialogDelete(cart._id)}><Delete /></IconButton>
                     </TableCell>
                     <TableCell align='right'>
@@ -74,25 +129,7 @@ export default function CartItems (props) {
                     </TableCell>
                     <TableCell align='right'>{cart.name_item}</TableCell>
                     <TableCell align='right'>
-                      <Grid hidden={0}>
-                        {cart.total_items}
-                      </Grid>
-                      <Grid hidden={1}>
-                        <Formik
-                          initialValues={{ total_items: cart.total_items }}
-                          validationSchema={{ total_items: Yup.number().required() }}
-                          validate={(value) => {
-                            console.log(value)
-                          }}
-                        >
-                          <Form>
-                            <CustomTextField size='small' type='number' name='total_items' label='Total Items' variant='outlined' style={{ width: '100px' }} component={TextField} />
-                            <Button size='small'>
-                              <Check />
-                            </Button>
-                          </Form>
-                        </Formik>
-                      </Grid>
+                      {cart.total_items}
                     </TableCell>
                     <TableCell align='right'>{cart.total_price}</TableCell>
                   </TableRow>
